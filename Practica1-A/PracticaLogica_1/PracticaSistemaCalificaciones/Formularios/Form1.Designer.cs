a4c0519fb3b7f1b0b7e899422f008453f25867a1
zDefcon21 <antholozadarequelme@gmail.com>
2026-09-19 18:26:19 -0500

    Amplía interfaz de formulario y agrega controles y eventos
    - Se añadieron textboxes para nombre y tres calificaciones, etiquetas, botones para agregar, modificar, eliminar y salir, y un DataGridView para mostrar registros.
    - Se aumentó el tamaño del formulario y se ajustó la disposición y estilo de los controles.
    - El constructor ahora inicializa el DataGridView con columnas y propiedades visuales, y desactiva el botón de minimizar.
    - Se agregaron stubs para los manejadores de eventos de todos los controles, incluyendo un diálogo de confirmación funcional para el botón "Salir".

--- Practica1-A/PracticaLogica_1/PracticaSistemaCalificaciones/Formularios/Form1.Designer.cs
+++ Practica1-A/PracticaLogica_1/PracticaSistemaCalificaciones/Formularios/Form1.Designer.cs
@@ -23,20 +23,211 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.tabla = new System.Windows.Forms.DataGridView();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(108, 130);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(246, 20);
+            this.txtNombre.TabIndex = 0;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombrE);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Alumno:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(186, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(672, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Calificaciones  Del Curso De Visual Studio 2026";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(374, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nota Unidad  1:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(374, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nota Unidad  2:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(374, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Nota Unidad  3:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(761, 131);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(132, 43);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregaR);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(910, 132);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(132, 43);
+            this.btnModificar.TabIndex = 7;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(1062, 131);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(132, 43);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(557, 131);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(145, 20);
+            this.txtNota1.TabIndex = 9;
+            this.txtNota1.TextChanged += new System.EventHandler(this.txtNota1_TextChanged);
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(557, 172);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(145, 20);
+            this.txtNota2.TabIndex = 10;
+            this.txtNota2.TextChanged += new System.EventHandler(this.txtNota2_TextChanged);
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(557, 220);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(145, 20);
+            this.txtNota3.TabIndex = 11;
+            this.txtNota3.TextChanged += new System.EventHandler(this.txtNota3_TextChanged);
+            // 
+            // tabla
+            // 
+            this.tabla.AllowUserToDeleteRows = false;
+            this.tabla.BackgroundColor = System.Drawing.Color.White;
+            this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla.Location = new System.Drawing.Point(171, 271);
+            this.tabla.Name = "tabla";
+            this.tabla.ReadOnly = true;
+            this.tabla.RowHeadersWidth = 47;
+            this.tabla.Size = new System.Drawing.Size(940, 532);
+            this.tabla.TabIndex = 12;
+            this.tabla.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tabla_CellContentClick);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(1084, 29);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(132, 43);
+            this.btnSalir.TabIndex = 13;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // Formulario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1052, 787);
+            this.ClientSize = new System.Drawing.Size(1228, 842);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.tabla);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
             this.Name = "Formulario";
             this.Text = "SistemaCalificaciones";
+            this.Load += new System.EventHandler(this.Formulario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.DataGridView tabla;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
 
--- Practica1-A/PracticaLogica_1/PracticaSistemaCalificaciones/Formularios/Form1.cs
+++ Practica1-A/PracticaLogica_1/PracticaSistemaCalificaciones/Formularios/Form1.cs
@@ -12,6 +12,86 @@ namespace PracticaSistemaCalificaciones {
     public partial class Formulario : Form {
         public Formulario() {
             InitializeComponent();
+            CenterToScreen();
+            MinimizeBox = false;
+            tabla.BorderStyle = BorderStyle.None;
+            BackColor = Color.White;
+            tabla.Font = new Font("Cascadia Code", 11, FontStyle.Bold);
+
+            tabla.Columns.Add("Nombre", "Nombre");
+            tabla.Columns.Add("Edad", "Edad");
+            tabla.Columns.Add("Cedula", "Cedula");
+            tabla.Columns.Add("Turno", "Turno");
+            tabla.Columns.Add("Nota 1", "Nota 1");
+            tabla.Columns.Add("Nota 2", "Nota 2");
+            tabla.Columns.Add("Nota 3", "Nota 3");
+            tabla.Columns.Add("Promedio Final", "Promedio Final");
         }
+
+        private void btnSalir_Click(object sender, EventArgs e) {
+
+            DialogResult opc = MessageBox.Show("Desea Salir Del Sistema?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+
+            if(opc == DialogResult.Yes) {
+                Application.Exit();
+            }
+
+        }
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void Formulario_Load(object sender, EventArgs e) {
+
+        }
+
+        private void txtNombrE(object sender, EventArgs e) {
+
+        }
+
+        private void btnAgregaR(object sender, EventArgs e) {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e) {
+
+        }
+
+        private void txtNota3_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void txtNota2_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void txtNota1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void tabla_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e) {
+
+        }
+
     }
 }
