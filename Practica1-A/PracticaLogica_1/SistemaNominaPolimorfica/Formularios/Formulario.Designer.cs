a4abb9c076ea28296ff7747d106ec73cee366d10
zDefcon21 <antholozadarequelme@gmail.com>
2026-09-25 14:15:40 -0500

    Implementa interfaz gráfica para gestión de empleados
    - Agrega etiquetas, cajas de texto, botones de opción, DataGridView y botón de registro al formulario.
    - Configura propiedades visuales y ajusta tamaño del formulario.
    - Declara variables privadas para los nuevos controles.
    - Permite capturar, mostrar y registrar datos de empleados en el sistema de nómina.

--- Practica1-A/PracticaLogica_1/SistemaNominaPolimorfica/Formularios/Formulario.Designer.cs
+++ Practica1-A/PracticaLogica_1/SistemaNominaPolimorfica/Formularios/Formulario.Designer.cs
@@ -23,20 +23,223 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtSueldoBase = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbdGerente = new System.Windows.Forms.RadioButton();
+            this.rdbVendedor = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtBonoAsignado = new System.Windows.Forms.TextBox();
+            this.txtVentas = new System.Windows.Forms.TextBox();
+            this.tabla = new System.Windows.Forms.DataGridView();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(293, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema De Nomina [POO]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Datos Generales";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(362, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sueldo Base:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(158, 135);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtSueldoBase
+            // 
+            this.txtSueldoBase.Location = new System.Drawing.Point(511, 136);
+            this.txtSueldoBase.Name = "txtSueldoBase";
+            this.txtSueldoBase.Size = new System.Drawing.Size(100, 20);
+            this.txtSueldoBase.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cascadia Code SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(62, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tipo Empleados";
+            // 
+            // rbdGerente
+            // 
+            this.rbdGerente.AutoSize = true;
+            this.rbdGerente.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbdGerente.Location = new System.Drawing.Point(69, 228);
+            this.rbdGerente.Name = "rbdGerente";
+            this.rbdGerente.Size = new System.Drawing.Size(90, 24);
+            this.rbdGerente.TabIndex = 7;
+            this.rbdGerente.TabStop = true;
+            this.rbdGerente.Text = "Gerente";
+            this.rbdGerente.UseVisualStyleBackColor = true;
+            // 
+            // rdbVendedor
+            // 
+            this.rdbVendedor.AutoSize = true;
+            this.rdbVendedor.Font = new System.Drawing.Font("Cascadia Code SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbVendedor.Location = new System.Drawing.Point(195, 228);
+            this.rdbVendedor.Name = "rdbVendedor";
+            this.rdbVendedor.Size = new System.Drawing.Size(99, 24);
+            this.rdbVendedor.TabIndex = 8;
+            this.rdbVendedor.TabStop = true;
+            this.rdbVendedor.Text = "Vendedor";
+            this.rdbVendedor.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cascadia Code SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(64, 267);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(221, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Datos Especificados";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cascadia Code SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(62, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 25);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Bono Asignado:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Cascadia Code SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(362, 319);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(155, 25);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Total Ventas:";
+            // 
+            // txtBonoAsignado
+            // 
+            this.txtBonoAsignado.Location = new System.Drawing.Point(225, 326);
+            this.txtBonoAsignado.Name = "txtBonoAsignado";
+            this.txtBonoAsignado.Size = new System.Drawing.Size(100, 20);
+            this.txtBonoAsignado.TabIndex = 12;
+            // 
+            // txtVentas
+            // 
+            this.txtVentas.Location = new System.Drawing.Point(523, 326);
+            this.txtVentas.Name = "txtVentas";
+            this.txtVentas.Size = new System.Drawing.Size(100, 20);
+            this.txtVentas.TabIndex = 13;
+            // 
+            // tabla
+            // 
+            this.tabla.BackgroundColor = System.Drawing.Color.White;
+            this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla.Location = new System.Drawing.Point(67, 371);
+            this.tabla.Name = "tabla";
+            this.tabla.Size = new System.Drawing.Size(770, 169);
+            this.tabla.TabIndex = 14;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(374, 216);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(132, 43);
+            this.btnRegistrar.TabIndex = 15;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // Formulario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(888, 724);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(863, 565);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.tabla);
+            this.Controls.Add(this.txtVentas);
+            this.Controls.Add(this.txtBonoAsignado);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rdbVendedor);
+            this.Controls.Add(this.rbdGerente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSueldoBase);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Formulario";
             this.Text = "Sistema De Nomina";
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtSueldoBase;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbdGerente;
+        private System.Windows.Forms.RadioButton rdbVendedor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBonoAsignado;
+        private System.Windows.Forms.TextBox txtVentas;
+        private System.Windows.Forms.DataGridView tabla;
+        private System.Windows.Forms.Button btnRegistrar;
     }
 }
 
